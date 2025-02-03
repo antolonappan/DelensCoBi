0f0597cff6466e2e35252e7bbe650fa9ec7b751e
Anto Idicherian Lonappan <antolonappan@icloud.com>
2025-02-02 21:25:26 -0800

    cmb
    Signed-off-by: Anto Idicherian Lonappan <antolonappan@icloud.com>

---
+++ dance/data.py
@@ -0,0 +1,84 @@
+# This file contains the data classes and functions to download the data files from the repository.
+
+# General imports
+import os
+from pickle import load
+from camb import read_ini
+from healpy import read_map
+from typing import Any, Optional
+from dataclasses import dataclass,field
+# Local imports
+from dance.utils import download_file
+from dance import mpi
+
+@dataclass
+class Data:
+    filename: str
+    _directory: Optional[str] = field(default=None, repr=False)
+    _galcut: Optional[int] = field(default=0, repr=False)
+
+
+    @property
+    def directory(self) -> Optional[str]:
+        return self._directory
+    
+    @property
+    def galcut(self) -> Optional[int]:
+        return self._galcut
+
+    @directory.setter
+    def directory(self, value: str) -> None:
+        if not os.path.isdir(value):
+            raise ValueError(f"The directory {value} does not exist.")
+        self._directory = value
+
+    @galcut.setter
+    def galcut(self, value: int) -> None:
+        if value < 0:
+            raise ValueError("The galcut value must be non-negative.")
+        self._galcut = value
+
+    def __dir__(self) -> str:
+        assert self.directory is not None, 'Directory is not set.'
+        return os.path.join(self.directory, 'Data')
+
+    @property
+    def fname(self) -> str:
+        directory = self.__dir__()
+        return os.path.join(directory, self.filename)
+
+    @property
+    def url(self) -> str:
+        return f"https://github.com/antolonappan/dance/releases/download/1.0/{self.filename}"
+    
+    
+
+    def __load__(self, fname: str) -> Any:
+        ext = fname.split('.')[-1]
+        match ext:
+            case 'fits':  
+                return read_map(fname, field=self._galcut) # type: ignore
+            case 'pkl':
+                return load(open(fname, 'rb'))
+            case 'ini':
+                return read_ini(fname)
+            case _:
+                raise ValueError(f'Unknown file extension: {ext}')
+
+    @property
+    def data(self) -> Any:
+        fname = self.fname
+        if os.path.isfile(fname):
+            return self.__load__(fname)
+        else:
+            if mpi.rank == 0:
+                os.makedirs(self.__dir__(), exist_ok=True)
+                download_file(self.url, fname)
+            mpi.barrier()
+            return self.__load__(fname)
+
+
+
+GAL_MASK = Data("binary_GAL_mask_N1024.fits")
+CAMB_INI = Data('cb.ini')
+SPECTRA = Data('spectra.pkl')
---
+++ dance/utils.py
@@ -0,0 +1,130 @@
+import requests
+import logging
+import numpy as np
+from tqdm import tqdm
+import healpy as hp
+
+class Logger:
+    def __init__(self, name: str, verbose: bool = False):
+        """
+        Initializes the logger.
+        
+        Parameters:
+        name (str): Name of the logger, typically the class name or module name.
+        verbose (bool): If True, set logging level to DEBUG, otherwise to WARNING.
+        """
+        self.logger = logging.getLogger(name)
+        
+        # Configure logging level based on verbosity
+        if verbose:
+            self.logger.setLevel(logging.DEBUG)
+        else:
+            self.logger.setLevel(logging.WARNING)
+        
+        # Prevent adding multiple handlers to the logger
+        if not self.logger.hasHandlers():
+            # Create console handler
+            ch = logging.StreamHandler()
+            ch.setLevel(logging.DEBUG)
+            
+            # Create formatter and add it to the handler
+            formatter = logging.Formatter('%(name)s : %(levelname)s - %(message)s')
+            ch.setFormatter(formatter)
+            
+            # Add handler to the logger
+            self.logger.addHandler(ch)
+
+    def log(self, message: str, level: str = 'info'):
+        """
+        Logs a message at the specified logging level.
+        
+        Parameters:
+        message (str): The message to log.
+        level (str): The logging level (debug, info, warning, error, critical).
+        """
+        level = level.lower()
+        if level == 'debug':
+            self.logger.debug(message)
+        elif level == 'info':
+            self.logger.info(message)
+        elif level == 'warning':
+            self.logger.warning(message)
+        elif level == 'error':
+            self.logger.error(message)
+        elif level == 'critical':
+            self.logger.critical(message)
+        else:
+            self.logger.info(message)
+
+
+def inrad(alpha: float) -> float:
+    """
+    Converts an angle from degrees to radians.
+
+    Parameters:
+    alpha (float): The angle in degrees.
+
+    Returns:
+    float: The angle in radians.
+    """
+    return np.deg2rad(alpha)
+
+def cli(cl: np.ndarray) -> np.ndarray:
+    """
+    Computes the inverse of each element in the input array `cl`.
+
+    Parameters:
+    cl (np.ndarray): Input array for which the inverse is calculated.
+                     Only positive values will be inverted; zeros and negative values will remain zero.
+
+    Returns:
+    np.ndarray: An array where each element is the inverse of the corresponding element in `cl`,
+                with zeros or negative values left unchanged.
+    """
+    ret = np.zeros_like(cl)
+    ret[np.where(cl > 0)] = 1.0 / cl[np.where(cl > 0)]
+    return ret
+
+
+def download_file(url, filename):
+    """Download a file with a progress bar."""
+    response = requests.get(url, stream=True)
+    total_size = int(response.headers.get('content-length', 0))
+    block_size = 1024 # 1 Kibibyte
+    t = tqdm(total=total_size, unit='iB', unit_scale=True, desc=f'Downloading {filename}')
+    with open(filename, 'wb') as file:
+        for data in response.iter_content(block_size):
+            t.update(len(data))
+            file.write(data)
+    t.close()
+
+
+def deconvolveQU(QU,beam):
+    """
+    Deconvolves a beam from a QU map.
+
+    Parameters:
+    QU (np.ndarray): The input QU map.
+    beam (np.ndarray): The beam to deconvolve.
+
+    Returns:
+    np.ndarray: The deconvolved QU map.
+    """
+    beam = np.radians(beam/60)
+    nside = hp.npix2nside(len(QU[0]))
+    elm,blm = hp.map2alm_spin(QU,2)
+    lmax = hp.Alm.getlmax(len(elm))
+    bl = hp.gauss_beam(beam,lmax=lmax,pol=True).T
+    hp.almxfl(elm,cli(bl[1]),inplace=True)
+    hp.almxfl(blm,cli(bl[2]),inplace=True)
+    return hp.alm2map_spin([elm,blm],nside,2,lmax)
+
+
+def change_coord(m, coord=['C', 'G']):
+    npix = m.shape[-1]
+    nside = hp.npix2nside(npix)
+    ang = hp.pix2ang(nside, np.arange(npix))
+    rot = hp.Rotator(coord=reversed(coord))
+    new_ang = rot(*ang)
+    new_pix = hp.ang2pix(nside, *new_ang)
+    return m[..., new_pix]
