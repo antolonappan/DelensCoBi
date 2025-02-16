18d0f128bf8406c23043b89ea0fc9a211d15d1fa
Anto Idicherian Lonappan <antolonappan@icloud.com>
2025-02-15 20:36:06 -0800

    delens aniso
    Signed-off-by: Anto Idicherian Lonappan <antolonappan@icloud.com>

--- dance/simulations/__init__.py
+++ dance/simulations/__init__.py
@@ -4,6 +4,7 @@ from .sky import Sky
 import numpy as np
 import healpy as hp
 from typing import Dict, Optional, Any, Union, List
+from plancklens import utils
 
 class mysims(object):
     def __init__(
@@ -40,5 +41,42 @@ class mysims(object):
         elm = self.get_sim_elm(idx)
         blm = self.get_sim_blm(idx)
         Q,U = hp.alm2map_spin([elm,blm], self.sky.nside, 2, lmax=self.sky.lmax)
+        del elm,blm
+        return Q ,U
+    
+class delensims:
+
+    def __init__(self, delens):
+        self.delens = delens
+        self.fle = utils.cli(delens.wf.cl_len['ee'][:4097]*delens.wf.ivfs.fel/delens.wf.ivfs.transf['e'])
+        self.flb = utils.cli(delens.wf.cl_len['bb'][:4097]*delens.wf.ivfs.fbl/delens.wf.ivfs.transf['b'])
+        self.lmax = self.delens.nside*3 - 1
+
+    def hashdict(self):
+        return {'sim_lib': 'none'}
+
+    def get_sim_tlm(self,idx):
+        lmax = 3*2048 - 1
+        return np.zeros(hp.Alm.getsize(lmax),dtype=np.complex64)
+
+    def get_sim_elm(self,idx):
+        e,b = self.delens.delens(idx)
+        del b
+        return hp.almxfl(e,self.fle)
+
+    def get_sim_blm(self,idx):
+        e,b = self.delens.delens(idx)
+        del e
+        return hp.almxfl(b,self.flb)
+    
+    def get_sim_tmap(self,idx):
+        tmap = self.get_sim_tlm(idx)
+        tmap = hp.alm2map(tmap,self.delens.nside)
+        return tmap
+    
+    def get_sim_pmap(self,idx):
+        elm = self.get_sim_elm(idx)
+        blm = self.get_sim_blm(idx)
+        Q,U = hp.alm2map_spin([elm,blm], self.delens.nside, 2, lmax=self.lmax)
         del elm,blm
         return Q ,U
